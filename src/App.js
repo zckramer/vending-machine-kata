bd45dec95b58d62a95d7fba3ba2651b0a9c0994f
Zack Kramer <zckramer83@gmail.com>
2020-11-10 15:54:27 -0500

    can press button to insert a quarter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import React, { useState } from 'react';
+import {createUseStyles} from 'react-jss';
+
+const styles = createUseStyles({
+  app: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
+
+const Quarter = ({
+  weight: 10,
+  size: .5
+})
+
+const Dime = ({
+  weight: 2,
+  size: .2
+})
+
+const Nickel = ({
+  weight: 5,
+  size: .4
+})
+
+const acceptableCoins = ({
+    quarter: {
+      weight: 10,
+      size: .5,
+      value: .25
+    },
+    dime: {
+      weight: 2,
+      size: .2,
+      value: .1
+    },
+    nickel: {
+      weight: 5,
+      size: .4,
+      value: .05
+    },
+    penny: {
+      weight: 1,
+      size: .2,
+      value: .01
+    }
+})
+
+
 
 function App() {
+  const [acceptedCoins, setAcceptedCoins] = useState([])
+  
+  function insertCoin (coinObject) {
+    const coinWeight = coinObject.weight;
+    const coinSize = coinObject.size;
+    // console.log("coin to determine: ", coinObject)
+    // console.log("acceptable parameters: size=",acceptableCoins.quarter.size, " weight=", acceptableCoins.quarter.weight)
+    if(coinSize === acceptableCoins.quarter.size && coinWeight === acceptableCoins.quarter.weight) {
+      console.log("inserted coin size = ", coinSize);
+      console.log("inserted coin weight = ", coinWeight);
+      console.log("this is a quarter");
+      setAcceptedCoins([coinObject])
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={styles.app}>
+      <Header vendingdata={acceptedCoins}/>
+      <button onClick={()=>insertCoin(Quarter)}>Insert Coin</button>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {createUseStyles} from 'react-jss';
+
+const styles = createUseStyles({
+    header: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignContent: 'center',
+        fontSize: 50,
+    }
+})
+
+const Header = (props) => {
+    const totalsData = props;
+    return (
+        <div className="Header" style={styles.header}>
+            {console.log(totalsData)}
+            <div className="Header__Money">
+                <div>Quarters: </div><p>{props.length}</p>
+                <div>Dimes: </div><p>{}</p>
+                <div>Nickels: </div><p>{}</p>
+                <div>Total Inserted: </div><p>total value</p>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
